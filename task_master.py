d2993abc07171acbe156e8f60c24f537322da634
shiyongjie <shiyongjie@neusoft.com>
2016-08-24 18:29:32 +0800

    2016-08-24 18:29

---
+++ task_master.py
@@ -0,0 +1,27 @@
+#在Thread和Process中，应当优选Process,因为Process更稳定，而且，Process可以分布到多台机器上，而Thread最多只能分布到同一台机器的多个CPU上
+#Python的multiprocessing模块不但支持多进程，其中managers子模块还支持把多进程分布到多台机器上。一个服务进程可以作为调度者，将任务分布到其他多个进程中，依靠网络通信。由于managers模块封装很好，不必了解网络通信的细节，就可以很容易地编写分布式多进程程序。
+#举个例子：如果我们已经有一个通过Queue通信的多进程程序在同一台机器上运行，现在，由于处理任务的进程任务繁重，希望把发送任务的进程和处理任务的进程分布到两台机器上。那么怎么用分布式进程实现？
+#原有的Queue可以继续使用，但是，通过managers模块把Queue通过网络暴露出去，就可以让其他机器的进程访问Queue了
+#我们先看服务进程，服务进程负责启动Queue,把Queue注册到网络上，然后往Queue里面写入任务
+import random,time,queue
+from multiprocessing.managers import BaseManager
+task_queue = queue.Queue()
+result_queue = queue.Queue()
+class QueueManager(BaseManager):
+    pass
+QueueManager.register('get_task_queue',callable=lambda:task_queue)
+QueueManager.register('get_result_queue',callable=lambda:result_queue)
+manager = QueueManager(address=('',5000),authkey=b'abc')
+manager.start()
+task = manager.get_task_queue()
+result = manager.get_result_queue()
+for i in range(10):
+    n = random.randint(0,10000)
+    print('Put task %d...' % n)
+    task.put(n)
+print('Try get results...')
+for i in range(10):
+    r = result.get(timeout=10)
+    print('Result:%s' % r)
+manager.shudown()
+print('master exit.')
